043d0730bcc8bf9b3931a7bc05b5ac53774c04a6
Yanky597 <64791155+Yanky597@users.noreply.github.com>
2021-07-17 23:50:54 -0400

    Add files via upload

---
+++ twoSumYteller.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        '''
+        using list comprehension we iterate through the list of nums and check if the target - currentValue
+        is in the list, if yes we return the current index +  the index of the second value.
+
+        This is my solution written out in the traditional way for better readability.
+
+        for i in range(len(nums)):
+            if((target-nums[i]) in nums and nums.index(target-nums[i]) != i):
+                return [i, nums.index(target-nums[i])]
+        '''
+
+        return [[i, nums.index(target - nums[i])] for i in range(len(nums)) if
+                ((target - nums[i]) in nums) and (nums.index(target - nums[i]) != i)][0]
