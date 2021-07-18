2d500dd2f7f5591456b352a0d63616238501791e
Yanky597 <64791155+Yanky597@users.noreply.github.com>
2021-07-17 23:53:41 -0400

    Update twoSumYteller.py

--- twoSumYteller.py
+++ twoSumYteller.py
@@ -2,6 +2,10 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
         '''
+        Y.Teller
+        
+        The following is my solution to the Leet code problem two sum
+        
         using list comprehension we iterate through the list of nums and check if the target - currentValue
         is in the list, if yes we return the current index +  the index of the second value.
 
